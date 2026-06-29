74f2bccd292263176c885525ac9dc7bc48274564
Luna Markink <markink365232@mijn.capabel.nl>
2026-06-29 17:07:02 +0200

    Added edit button to clients page

--- obj/Debug/net10.0/PetCarePro.AssemblyInfo.cs
+++ obj/Debug/net10.0/PetCarePro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502a526e2d7ffdf90b0034999754ff6a6caa5925")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61c4a0e26e39d6465bf94ab8345db818c9054087")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
