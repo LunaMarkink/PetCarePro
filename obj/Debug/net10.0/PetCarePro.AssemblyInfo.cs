9bab041c3ecc643af2501d50b23b7750d4dcfed3
Luna Markink <markink365232@mijn.capabel.nl>
2026-06-29 15:04:52 +0200

    Added pet class

--- obj/Debug/net10.0/PetCarePro.AssemblyInfo.cs
+++ obj/Debug/net10.0/PetCarePro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57bd285c2e9a2caf8fd1872f8e45abdf54334e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
