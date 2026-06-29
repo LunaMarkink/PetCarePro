67dd26f9eb3f64c81befddd3b26e51b22438a32d
Luna Markink <markink365232@mijn.capabel.nl>
2026-06-29 15:23:07 +0200

    Added stay class

---
+++ Classes/Stay.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace PetCareProBlazor.Classes
+{
+    // Stay
+    public class Stay
+    {
+        public int StayId { get; set; }
+
+        // Relationship with pet
+        public int PetId { get; set; }
+        public Pet Pet { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public string KennelNumber { get; set; }
+        public string Status { get; set; }
+    }
+}
