2513c6d695b5f5f2dce4c005ae6d53e33bd2d207
Luna Markink <markink365232@mijn.capabel.nl>
2026-07-07 09:53:14 +0200

    Edited edit user page

--- obj/Debug/net10.0/PetCarePro.AssemblyInfo.cs
+++ obj/Debug/net10.0/PetCarePro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2557e3f7e26fc662f1b2eca20de5d462fd8ee6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d679258a8f637a26e8483082c59780a77b64ae07")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
