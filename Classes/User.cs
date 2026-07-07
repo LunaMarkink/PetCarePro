9f4a11c175ecb331a4f586792f7ab19b0dc52975
Luna Markink <markink365232@mijn.capabel.nl>
2026-07-07 10:05:29 +0200

    Edited user.cs

--- Classes/User.cs
+++ Classes/User.cs
@@ -7,10 +7,10 @@ namespace PetCarePro.Classes
     {
         public int UserId { get; set; }
 
-        public string Username { get; set; }
+        public string Username { get; set; } = "";
 
-        public string Password { get; set; }
+        public string Password { get; set; } = "";
 
-        public string Role { get; set; }
+        public string Role { get; set; } = "user"; 
     }
 }
