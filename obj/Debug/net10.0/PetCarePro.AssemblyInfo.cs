c4da7e32443fd4a6a40996853e523b951c0b86b1
Luna Markink <markink365232@mijn.capabel.nl>
2026-07-14 11:45:19 +0200

    Added validation to edit stay page

--- obj/Debug/net10.0/PetCarePro.AssemblyInfo.cs
+++ obj/Debug/net10.0/PetCarePro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8608a442fa0593f2332dfa133c32887b1658defa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d46def35a967ee7735dd2e367917a6264538b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
