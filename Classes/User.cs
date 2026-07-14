fe45cf085d02ebe290d2937cffbc7ffe51e63387
Luna Markink <markink365232@mijn.capabel.nl>
2026-07-14 13:17:50 +0200

    Added validation to edit user page

--- Classes/User.cs
+++ Classes/User.cs
@@ -1,16 +1,23 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PetCarePro.Classes
 {
-    // User
     public class User
     {
         public int UserId { get; set; }
 
-        public string Username { get; set; } = "";
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(30, ErrorMessage = "Username cannot exceed 30 characters.")]
+        public string Username { get; set; } = string.Empty;
 
-        public string Password { get; set; } = "";
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 6,
+            ErrorMessage = "Password must contain between 6 and 100 characters.")]
+        public string Password { get; set; } = string.Empty;
 
-        public string Role { get; set; } = "user"; 
+        [Required(ErrorMessage = "Role is required.")]
+        [RegularExpression("Administrator|Employee",
+            ErrorMessage = "Role must be Administrator or Employee.")]
+        public string Role { get; set; } = "Employee";
     }
 }
