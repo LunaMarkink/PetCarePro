2cfaf45c831069972d463cfa00ac1d35360b4835
Luna Markink <markink365232@mijn.capabel.nl>
2026-07-14 14:34:07 +0200

    add pet search and owner display

--- obj/Debug/net10.0/PetCarePro.AssemblyInfo.cs
+++ obj/Debug/net10.0/PetCarePro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+650b8cd99fb58257faeda709d999cc28fe01b36f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e30e33ad63239a35b61308d73cfe0aeebbd1a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetCarePro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
