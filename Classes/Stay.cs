6d46def35a967ee7735dd2e367917a6264538b7d
Luna Markink <markink365232@mijn.capabel.nl>
2026-07-14 11:31:50 +0200

    Edited stay validation

--- Classes/Stay.cs
+++ Classes/Stay.cs
@@ -6,15 +6,15 @@ namespace PetCarePro.Classes
     {
         public int StayId { get; set; }
 
-        [Range(1, int.MaxValue, ErrorMessage = "Selecteer een huisdier.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Select a pet.")]
         public int PetId { get; set; }
 
         public Pet Pet { get; set; } = null!;
 
-        [Required(ErrorMessage = "Check-in datum is verplicht.")]
+        [Required(ErrorMessage = "Check-in date is required.")]
         public DateTime StartDate { get; set; } = DateTime.Today;
 
-        [Required(ErrorMessage = "Check-out datum is verplicht.")]
+        [Required(ErrorMessage = "Check-out date is required.")]
         public DateTime EndDate { get; set; } = DateTime.Today.AddDays(1);
 
         [StringLength(50)]
