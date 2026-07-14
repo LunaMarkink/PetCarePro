5f8de55f6ae7fb5695bd66d66da0cf16f2ef66e5
Luna Markink <markink365232@mijn.capabel.nl>
2026-07-14 10:26:28 +0200

    Add validation attributes to Client model

--- Classes/Client.cs
+++ Classes/Client.cs
@@ -1,19 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PetCarePro.Classes
 {
     public class Client
     {
         public int ClientId { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
 
-        // Relationship with pets
-        public List<Pet> Pets { get; set; }
+        [Required(ErrorMessage = "Naam is verplicht.")]
+        [StringLength(100, ErrorMessage = "De naam mag maximaal 100 tekens bevatten.")]
+        public string Name { get; set; } = string.Empty;
 
-        public Client()
-        {
-            Pets = new List<Pet>();
-        }
+        [Required(ErrorMessage = "Adres is verplicht.")]
+        [StringLength(200, ErrorMessage = "Het adres mag maximaal 200 tekens bevatten.")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Telefoonnummer is verplicht.")]
+        [Phone(ErrorMessage = "Vul een geldig telefoonnummer in.")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "E-mailadres is verplicht.")]
+        [EmailAddress(ErrorMessage = "Vul een geldig e-mailadres in.")]
+        public string Email { get; set; } = string.Empty;
+
+        public List<Pet> Pets { get; set; } = new();
     }
 }
